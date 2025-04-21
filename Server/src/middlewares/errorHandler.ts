14e9d280f345404729f84f22c18601f9c9630beb
Aidan Webster <55215693+ayejay-nz@users.noreply.github.com>
2025-04-22 00:35:10 +1200

    Add support for MulterError in errorHandler middleware

--- Server/src/middlewares/errorHandler.ts
+++ Server/src/middlewares/errorHandler.ts
@@ -3,9 +3,10 @@ import { HTTP_STATUS_CODE, API_ERROR_MESSAGE, API_ERROR_CODE } from '../constant
 import ApiError from '../utils/apiError';
 import { ApiErrorResponse } from '../dataTypes/apiErrorResponse';
 import config from '../config/config';
+import { MulterError } from 'multer';
 
 const errorHandler = (
-    error: Error | ApiError,
+    error: Error | ApiError | MulterError,
     req: Request,
     res: Response,
     _next: NextFunction
@@ -27,6 +28,27 @@ const errorHandler = (
             isDev || error.exposeDetails
         ) ? error.details : undefined;
         responseExposeDetails = error.exposeDetails;
+    } else if (error instanceof MulterError) {
+        if (error.code === 'LIMIT_FILE_SIZE') {
+            responseStatus = HTTP_STATUS_CODE.CONTENT_TOO_LARGE;
+            responseMessage = API_ERROR_MESSAGE.fileTooLarge;
+            responseErrorCode = API_ERROR_CODE.FILE_TOO_LARGE;
+            responseDetails = { 
+                multerErrorCode: error.code,
+                multerErrorMessage: error.message,
+                multerErrorField: error.field
+            };
+        } else {
+            responseStatus = HTTP_STATUS_CODE.BAD_REQUEST;
+            responseMessage = API_ERROR_MESSAGE.badRequest;
+            responseErrorCode = API_ERROR_CODE.BAD_REQUEST;
+            responseDetails = { 
+                multerErrorCode: error.code,
+                multerErrorMessage: error.message,
+                multerErrorField: error.field
+            };
+        }
+        responseExposeDetails = false;
     } else {
         responseStatus = HTTP_STATUS_CODE.SERVER_ERROR;
         responseMessage = API_ERROR_MESSAGE.serverError;
