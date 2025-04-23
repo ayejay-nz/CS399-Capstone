924641f07ae8231b0ba0b0adbe8f53b1244b7335
Aidan Webster <55215693+ayejay-nz@users.noreply.github.com>
2025-04-23 16:38:42 +1200

    Add API endpoint for asset file uploads with png and jpeg validation
    Implements a /upload endpoint which uses the uploadAssetFile middleware to handle uploads.
    This ensures any provided file is a png or jpeg and less than 1MB, and triggers and appropriate error response otherwise.
    
    No actual parsing or handling of assets has been implemented yet - currently serves as a placeholder for future logic

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -6,6 +6,7 @@ import examSourceRoutes from './routes/examSourceRoutes';
 import coverPageRoutes from './routes/coverPageRoutes';
 import answerKeyRoutes from './routes/answerKeyRoutes';
 import teleformDataRoutes from './routes/teleformRoutes';
+import assetRoutes from './routes/assetRoutes';
 
 const app = express();  
 
@@ -21,6 +22,7 @@ app.use(`${config.server.apiPrefix}/exam-source`, examSourceRoutes);
 app.use(`${config.server.apiPrefix}/cover-page`, coverPageRoutes);
 app.use(`${config.server.apiPrefix}/answer-key`, answerKeyRoutes);
 app.use(`${config.server.apiPrefix}/teleform-data`, teleformDataRoutes);
+app.use(`${config.server.apiPrefix}/asset`, assetRoutes);
 
 app.use(errorHandler);  
 
