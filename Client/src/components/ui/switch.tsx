a516619f87ce75fd997f48ec264e208322ce7d76
Don <donsqlin@gmail.com>
2025-05-04 17:19:37 +1200

    update colour
    add more contrast so selected option is more clear

--- Client/src/components/ui/switch.tsx
+++ Client/src/components/ui/switch.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react"
 import * as SwitchPrimitives from "@radix-ui/react-switch"
-
 import { cn } from "@/src/lib/utils"
 
 const Switch = React.forwardRef<
@@ -11,7 +10,8 @@ const Switch = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
+      "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-offset-2 focus-visible:ring-offset-black disabled:cursor-not-allowed disabled:opacity-50",
+      "data-[state=unchecked]:bg-[#27272A] data-[state=checked]:bg-[#FAFAFA]",
       className,
     )}
     {...props}
@@ -19,7 +19,9 @@ const Switch = React.forwardRef<
   >
     <SwitchPrimitives.Thumb
       className={cn(
-        "pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0",
+        "pointer-events-none block h-5 w-5 rounded-full shadow-lg transition-transform",
+        "bg-[#09090B]",
+        "data-[state=unchecked]:translate-x-0 data-[state=checked]:translate-x-5",
       )}
     />
   </SwitchPrimitives.Root>
