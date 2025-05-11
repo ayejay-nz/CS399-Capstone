a6f3692532198fdf8aa1079a1af2e4bc8aaca777
Aidan Webster <55215693+ayejay-nz@users.noreply.github.com>
2025-05-12 03:45:12 +1200

    Finish parseAnswerKeyXLSX parser logic
    - Still need to finalise error handling, testing, and documentation

--- Server/src/parsers/answerKeyParser.ts
+++ Server/src/parsers/answerKeyParser.ts
@@ -1,14 +1,23 @@
-import { Column, Workbook, Worksheet } from 'exceljs';
-import { AnswerKey, VersionSolution } from '../dataTypes/answerKey';
+import { Workbook } from 'exceljs';
+import {
+    AnswerKey,
+    AnswerKeyQuestion,
+    QuestionSolution,
+    VersionSolution,
+} from '../dataTypes/answerKey';
+import ParserError from '../utils/parserError';
+import ApiError from '../utils/apiError';
 
 // '1234' -> [1, 2, 3, 4]
 function parseSequence(sequence: unknown): number[] {
     return String(sequence)
         .trim()
         .split('')
-        .map((d) => Number(d));
+        .map((d) => Number(d))
+        .filter((n) => !isNaN(n));
 }
 
+// 1 - > [1], '1,2,3' -> [1, 2, 3]
 function parseAnswers(answers: unknown): number[] {
     if (typeof answers === 'number') return [answers];
 
@@ -18,29 +27,142 @@ function parseAnswers(answers: unknown): number[] {
         .filter((n) => !isNaN(n));
 }
 
-async function parseSourceQuestions(source: Worksheet) {
-    try {
-        const questionIdCol = source.getColumn(1);
-        const questionStemCol = source.getColumn(2);
-        const markWeightCol = source.getColumn(3);
-        let optionCols: Column[] = [];
-        // Get the 10 option columns
-        for (let optionIdx = 1; optionIdx <= 10; optionIdx++) {
-            optionCols.push(source.getColumn(3 + optionIdx));
-        }
-        
-        const questionCount = questionIdCol.values
-    } catch (err) {}
+function parseSourceQuestionsFromWorkbook(wb: Workbook): AnswerKeyQuestion[] {
+    const sourceSheet = wb.getWorksheet('Source');
+    if (!sourceSheet) throw new ApiError(); // TODO -- SET ERROR CODES
+
+    const header = sourceSheet?.getRow(1);
+    if (!header) throw new ApiError(); // TODO -- SET ERROR CODES
+
+    const columns: {
+        questionId?: number;
+        questionStem?: number;
+        markWeight?: number;
+        optionColumns: { idx: number }[];
+    } = { optionColumns: [] };
+
+    // Extract relevant column information
+    header.eachCell((cell, idx) => {
+        const cellTxt = String(cell.value ?? '')
+            .trim()
+            .toLowerCase();
+        if (cellTxt === 'questionid') columns.questionId = idx;
+        else if (cellTxt === 'questionstem') columns.questionStem = idx;
+        else if (cellTxt === 'markweight') columns.markWeight = idx;
+        else if (/^[0..9]$/.test(cellTxt)) columns.optionColumns.push({ idx });
+    });
+
+    const questions: AnswerKeyQuestion[] = [];
+
+    // Extract question information
+    sourceSheet.eachRow((row, idx) => {
+        if (idx === 1) return;
+        const questionIdCell = row.getCell(columns.questionId!).value;
+        const questionStemCell = row.getCell(columns.questionStem!).value;
+        const markWeightCell = row.getCell(columns.markWeight!).value;
+
+        const options = columns.optionColumns
+            .map((col) => String(row.getCell(col.idx).value ?? '').trim())
+            .filter((option) => option !== '');
+
+        questions.push({
+            marks: Number(markWeightCell),
+            id: Number(questionIdCell),
+            feedback: {}, // Set up proper feedback later
+            content: String(questionStemCell),
+            options: options,
+        });
+    });
+
+    return questions;
+}
+
+function getVersionsFromSheet1(wb: Workbook): string[] {
+    const sheet1 = wb.getWorksheet('Sheet1');
+    if (!sheet1) throw new ApiError(); // TODO -- SET ERROR CODES
+
+    const versions: string[] = [];
+
+    // Get version column index
+    const header = sheet1.getRow(1);
+    let versionColumnIdx = 1;
+    header.eachCell((cell, cellIdx) => {
+        const cellTxt = String(cell.value ?? '')
+            .trim()
+            .toLowerCase();
+
+        if (cellTxt === 'versions') versionColumnIdx = cellIdx;
+    });
+
+    // Get version numbers
+    sheet1.eachRow((row, rowIdx) => {
+        if (rowIdx === 1) return; // Skip header
+        const versionNumber = row.getCell(versionColumnIdx).value;
+        if (versionNumber != null) versions.push(String(versionNumber).trim());
+    });
+
+    return versions;
 }
 
 export async function parseAnswerKeyXLSX(buffer: Buffer): Promise<AnswerKey> {
     const workbook = new Workbook();
     await workbook.xlsx.load(buffer);
 
-    const versionSolutions: VersionSolution[] = [];
+    const sourceQuestions = parseSourceQuestionsFromWorkbook(workbook);
+    const versionNames = getVersionsFromSheet1(workbook);
+    let versionSolutions: VersionSolution[] = [];
 
-    for (const sheet of workbook.worksheets) {
-        if (sheet.name === 'Source') {
-        }
-    }
+    versionNames.forEach((name) => {
+        const sheet = workbook.getWorksheet(name);
+        if (!sheet) throw new ApiError(); // TODO -- SET UP ERROR CODES
+
+        const header = sheet.getRow(1);
+
+        const columns: {
+            questionId?: number;
+            answer?: number;
+            markWeight?: number;
+            optionSequences?: number;
+        } = {};
+
+        // Extract relevant column indexes
+        header.eachCell((cell, idx) => {
+            const cellTxt = String(cell.value ?? '')
+                .trim()
+                .toLowerCase();
+            if (cellTxt === 'questionid') columns.questionId = idx;
+            else if (cellTxt === 'answer') columns.answer = idx;
+            else if (cellTxt === 'markweight') columns.markWeight = idx;
+            else if (cellTxt === 'optionsequences') columns.optionSequences = idx;
+        });
+
+        const questionSolutions: QuestionSolution[] = [];
+        sheet.eachRow((row, idx) => {
+            if (idx === 1) return;
+
+            const questionIdCell = row.getCell(columns.questionId!).value;
+            const answerCell = row.getCell(columns.answer!).value;
+            const markWeightCell = row.getCell(columns.markWeight!).value;
+            const optionSequenceCell = row.getCell(columns.optionSequences!).value;
+
+            questionSolutions.push({
+                questionId: Number(questionIdCell),
+                answers: parseAnswers(answerCell),
+                mark: Number(markWeightCell),
+                optionSequence: parseSequence(optionSequenceCell),
+            });
+        });
+
+        versionSolutions.push({
+            versionNumber: name,
+            questionSolutions: questionSolutions,
+        });
+    });
+
+    const answerKey = {
+        source: sourceQuestions,
+        versionSolutions: versionSolutions,
+    };
+
+    return answerKey;
 }
