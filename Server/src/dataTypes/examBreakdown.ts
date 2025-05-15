340f15f76e21dee6160a8a5415644a8bb5491d45
Aidan Webster <55215693+ayejay-nz@users.noreply.github.com>
2025-05-15 20:25:51 +1200

    Add `examMarks` field to examBreakdown `Summary` interface

--- Server/src/dataTypes/examBreakdown.ts
+++ Server/src/dataTypes/examBreakdown.ts
@@ -4,6 +4,7 @@ export interface Summary {
     median: number;
     upperQuartile: number;
     highestScore: number;
+    examMarks: number;
     studentScores: number[];
     lowestScoringQuestions: number[]; // array of question ids with the lowest mark
 }
