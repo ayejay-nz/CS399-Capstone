decde82e091a153fb06721b7e45c433bb3ba1e00
Don <donsqlin@gmail.com>
2025-05-29 21:54:49 +1200

    fix imagine import

--- Client/src/app/mark-mcq/page.tsx
+++ Client/src/app/mark-mcq/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { ImageUpload } from "../../components/ui/image-upload";
 import Link from "next/link";
 import { Button } from "../../components/ui/button";
